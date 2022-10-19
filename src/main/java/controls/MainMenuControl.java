ab883d81f35164a760d8d45eabe40fe23cfdb71b
Henry Akgz <akgz.henry@gmail.com>
2022-10-19 14:35:25 -0500

    se agrego una hoja de estilos css

--- src/main/java/controls/MainMenuControl.java
+++ src/main/java/controls/MainMenuControl.java
@@ -1,4 +1,12 @@
 package controls;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainMenuControl {
+    @FXML
+    Button close_butto;
+
+    @FXML
+    Button shrink_button;
 }
